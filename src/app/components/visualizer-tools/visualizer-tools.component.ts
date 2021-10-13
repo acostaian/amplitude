6f28339e49c27a63b8a7b73d121ed0838ab2650b
IanAcosta7 <ianacosta700@gmail.com>
2021-10-13 02:17:11 -0300

    Modified tools style

--- src/app/components/visualizer-tools/visualizer-tools.component.ts
+++ src/app/components/visualizer-tools/visualizer-tools.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizerToolsComponent implements OnInit {
 
+  public toolsNavClass: string = 'visualizer-nav';
+  public toolsArrowClass: string = 'visualizer-tools-arrow visualizer-tools-arrow-normal';
+
+  private _toolsShown: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showTools() {
+    this._toolsShown = !this._toolsShown;
+
+    this.toolsNavClass = this._toolsShown ? 'visualizer-nav' : 'visualizer-nav visualizer-nav-hidden';
+    this.toolsArrowClass = this._toolsShown ? 'visualizer-tools-arrow visualizer-tools-arrow-normal' : 'visualizer-tools-arrow visualizer-tools-arrow-inverse';
   }
 
 }
